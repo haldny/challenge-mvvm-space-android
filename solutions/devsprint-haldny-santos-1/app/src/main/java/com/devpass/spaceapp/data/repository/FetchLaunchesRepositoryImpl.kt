9689a533f2b8b15718e6d15def3ddcc399639169
joycesouzs <joycejfs@hotmail.com>
2023-08-15 18:10:43 -0300

    add const code review

--- solutions/devsprint-haldny-santos-1/app/src/main/java/com/devpass/spaceapp/data/repository/FetchLaunchesRepositoryImpl.kt
+++ solutions/devsprint-haldny-santos-1/app/src/main/java/com/devpass/spaceapp/data/repository/FetchLaunchesRepositoryImpl.kt
@@ -6,12 +6,15 @@ import com.devpass.spaceapp.data.datasource.remote.model.QueryParams
 import com.devpass.spaceapp.data.datasource.remote.model.SortRequest
 import com.devpass.spaceapp.data.datasource.remote.source.RemoteDataSource
 
+private const val LIMIT = 10
+private const val FLIGHT_NUMBER = "asc"
+
 class FetchLaunchesRepositoryImpl(
     private val remoteDataSource: RemoteDataSource,
 ) : FetchLaunchesRepository {
     override suspend fun getsLaunches(): LaunchesPage {
         return remoteDataSource.getsLaunches(
-            QueryParams(OptionsRequest(SortRequest("asc"), 10))
+            QueryParams(OptionsRequest(SortRequest(FLIGHT_NUMBER), LIMIT))
         )
     }
 }
